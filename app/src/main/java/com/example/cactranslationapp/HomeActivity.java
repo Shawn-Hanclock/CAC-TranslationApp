f8099fbc2ced65c3ed7e3c839730e5e582257537
Shawn Hancock <90814768+Shawn-Hanclock@users.noreply.github.com>
2023-10-31 20:34:55 -0700

    Styled Some of the pages.

--- app/src/main/java/com/example/cactranslationapp/HomeActivity.java
+++ app/src/main/java/com/example/cactranslationapp/HomeActivity.java
@@ -3,19 +3,24 @@ package com.example.cactranslationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.button.MaterialButton;
 
 public class HomeActivity extends AppCompatActivity {
     private MaterialButton toMainBtn;
+    private ImageView backgroundGlobe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        backgroundGlobe = findViewById(R.id.globeBack_imgV);
+        backgroundGlobe.setColorFilter(Color.argb(200, 255, 255, 255));
         toMainBtn = findViewById(R.id.toMainActivity_btn);
         toMainBtn.setOnClickListener(new View.OnClickListener() {
             @Override
